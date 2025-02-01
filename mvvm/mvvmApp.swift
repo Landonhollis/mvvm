c74122d184ee60a1b9333d6670699556f5417376
Landonhollis <landonhollis2006@gmail.com>
2025-02-01 14:50:12 -0500

    Initial Commit

---
+++ mvvm/mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmApp.swift
+//  mvvm
+//
+//  Created by Landon Hollis on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mvvmTests/mvvmTests.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmTests.swift
+//  mvvmTests
+//
+//  Created by Landon Hollis on 2/1/25.
+//
+
+import Testing
+@testable import mvvm
+
+struct mvvmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
